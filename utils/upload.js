0bc3d6644ce7caf3d7ff0a772edd91c1a1cface3
fabioabucejojr <gtxon3k@gmail.com>
2023-01-29 11:47:39 +0800

    Minor edit for multer and pool

--- utils/upload.js
+++ utils/upload.js
@@ -1,9 +1,24 @@
+const express = require("express");
 const router = require("express").Router();
 const multer = require("multer");
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
 const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./uploads");
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    },
+});
+
 
 //multer middleware
-const upload = multer( { storage: storage })
+const upload = multer({ storage: storage });
 
 router.post('/upload', upload.single('my-image'), async (req, res) => {
 
@@ -29,3 +44,5 @@ router.get('/photos', async (req, res) => {
         console.log(error.message)
     }
 })
+
+module.exports = router;
