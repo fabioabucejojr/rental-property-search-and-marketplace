483932f6bdc7bfb655f7db808898704bf8aae8c3
fabioabucejojr <gtxon3k@gmail.com>
2023-01-30 21:34:38 +0800

    Added Home function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,51 @@
-
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Home.css";
 import SearchBar from "../components/PropertySearch";
+import Map from "../components/Map";
+import PropertyList from "../components/AddPropertyListing";
 
 function Home() {
+  const [location, setLocation] = React.useState(null);
+  const [properties, setProperties] = React.useState([]);
+
+  const handleSearch = (location) => {
+    setLocation(location);
+    // Fetch properties near the location
+    // ...
+    // Set the properties in the state
+    setProperties(properties);
+  };
+
   return (
     <>
-
-    <div className="home">
-      <div className="inner">
-        <div className="result">
-          <div className="resbox"></div>
-          <SearchBar />
+      <div className="home">
+        <div className="inner">
+          <div className="result">
+            <div className="resbox">
+              {location ? (
+                <>
+                  <Map location={location} />
+                  <PropertyList properties={properties} />
+                </>
+              ) : (
+                <SearchBar onSearch={handleSearch} />
+              )}
+            </div>
+          </div>
+        </div>
+        <div className="btns">
+          {location && (
+            <>
+              <button>Filter by price</button>
+              <button>Filter by type</button>
+              <button>Filter by size</button>
+            </>
+          )}
         </div>
       </div>
-      <div className="btns">
-
-      </div>
-    </div>
     </>
-
-  )
+  );
 }
 
 export default Home;
