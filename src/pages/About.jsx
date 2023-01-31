627f8b914231e6953c6eb6456b2bfffa2f2314bd
fabioabucejojr <gtxon3k@gmail.com>
2023-02-01 06:50:40 +0800

    Added Lorem Ipsum

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,8 +5,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const About = () => {
     return (
         <section className='hero'>
-            
+
             <h1>Welcome to About Page</h1>
+            <p>Sint duis ea tempor velit nisi ad do eiusmod aliqua. Eiusmod elit magna proident veniam anim amet exercitation adipisicing adipisicing ullamco. Laboris sunt eu dolore nisi officia aliquip irure excepteur. Est id in dolor proident sint irure nostrud consequat nulla laboris officia mollit irure. Sint duis ea tempor velit nisi ad do eiusmod aliqua. Eiusmod elit magna proident veniam anim amet exercitation adipisicing adipisicing ullamco. Laboris sunt eu dolore nisi officia aliquip irure excepteur. Est id in dolor proident sint irure nostrud consequat nulla laboris officia mollit irure</p>
+
+            <p>Ipsum consequat fugiat officia elit duis tempor aute excepteur reprehenderit reprehenderit officia. Incididunt Lorem proident ullamco qui proident do. Non ut id incididunt irure excepteur est irure ut sint fugiat. Sint duis ea tempor velit nisi ad do eiusmod aliqua. Eiusmod elit magna proident veniam anim amet exercitation adipisicing adipisicing ullamco. Laboris sunt eu dolore nisi officia aliquip irure excepteur. Est id in dolor proident sint irure nostrud consequat nulla laboris officia mollit irure</p>
+
+            <p>Nisi proident magna consectetur qui cillum consectetur sunt quis. Excepteur aute sit officia anim occaecat ea esse ad deserunt id mollit. Eu labore ut esse laborum voluptate labore. Sint duis ea tempor velit nisi ad do eiusmod aliqua. Eiusmod elit magna proident veniam anim amet exercitation adipisicing adipisicing ullamco. Laboris sunt eu dolore nisi officia aliquip irure excepteur. Est id in dolor proident sint irure nostrud consequat nulla laboris officia mollit irure</p>
+
         </section>
     )
 }
