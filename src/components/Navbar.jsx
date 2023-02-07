be362f0f7dccac29ccc7822e13c92b3a1ea6dc04
fabioabucejojr <gtxon3k@gmail.com>
2023-02-07 19:06:08 +0800

    Modified NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,45 +1,20 @@
-import React, { useState } from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-import "./navbar.css";
-import { FaBars } from "react-icons/fa";
-import { ImCross } from "react-icons/im";
-// import Logo from "../../src/images/logo.svg";
-import logo from '../../src/images/logo.png';
-// import { ReactComponent as Logo } from ".../logo.png";
 
-const Navbar = () => {
-    const [mobile, setMobile] = useState(false)
-
-    return (
-        <nav className='navbar'>
-            <img src={logo} className="App-logo" alt="logo" /> HRental App
-
-                <ul className={mobile? "nav-links-mobile" : "nav-links"} onClick={() =>setMobile(false)}>
-                    <Link to='/'>
-                        <li className='lnkNavMenu'>Marketplace</li>
-                    </Link>
-                    <Link to='/about'>
-                        <li>About</li>
-                    </Link>
-                    <Link to='/getstarted'>
-                        <li>Get Started</li>
-                    </Link>
-                    <Link to='/contact'>
-                        <li>Contact Us</li>
-                    </Link>
-                    {/* <Link to='/register'>
-                        <li>Register</li>
-                    </Link> */}
-                    <Link to='/login'>
-                        <li>Login</li>
-                    </Link>
-                </ul>
-                <button className='mobile-menu-icon' onClick={()=>setMobile(!mobile)}>{mobile ? <ImCross /> : <FaBars />}
-                    
-                </button>
-            
+export default function Navbar() {
+  return (
+    <div>
+      <div>
+        <nav>
+          <a href="/"><img src="../images/logo.png" className="nav--logo" alt="logo" /></a>
+          <Link className="px-4" to="/register">Post Your Property Listing FREE!</Link>
+          <Link className="px-4" to="/marketplace">Marketplace</Link>
+          <Link className="px-4" to="/about">About</Link>
+          <Link className="px-4" to="/contact">Contact</Link>
+          <Link className="px-4" to="/login">Login</Link>
         </nav>
-    )
-}
+      </div>
 
-export default Navbar;
+    </div>
+  )
+}
