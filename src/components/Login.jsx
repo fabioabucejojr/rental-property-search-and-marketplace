3972856992ed7f8b5e7671f106ddcb5bcd552ce6
fabioabucejojr <gtxon3k@gmail.com>
2023-03-10 07:59:53 +0800

    Added minor changes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -20,12 +20,13 @@ import 'react-toastify/dist/ReactToastify.css';
 import UserDashboard from "./UsrDashboard";
 import AdminDashboard from "./Dashboard";
 
+
 function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
       <Link color="inherit" href="https://hrentalapp.com/">
-        KMS-House Rental Application
+        KMS-House Rental App
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -88,97 +89,99 @@ const Login = ({ setAuth }) => {
     };
 
   return (
+    <div>
 
-    <ThemeProvider theme={theme}>
+      <ThemeProvider theme={theme}>
 
-      <Grid container component="main" sx={{ height: '100vh' }}>
-        <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random/?RealEstate&1)',
-            backgroundRepeat: 'no-repeat',
-            backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          }}
-        />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
+        <Grid container component="main" sx={{ height: '100vh' }}>
+          <CssBaseline />
+          <Grid
+            item
+            xs={false}
+            sm={4}
+            md={7}
             sx={{
-              my: 8,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
+              backgroundImage: 'url(https://source.unsplash.com/random/?RealEstate&1)',
+              backgroundRepeat: 'no-repeat',
+              backgroundColor: (t) =>
+                t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
             }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
-            <Box component="form" noValidate onSubmit={onSubmitForm} sx={{ mt: 1 }}>
+          />
+          <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+            <Box
+              sx={{
+                my: 8,
+                mx: 4,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Sign in
+              </Typography>
+              <Box component="form" noValidate onSubmit={onSubmitForm} sx={{ mt: 1 }}>
 
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="user_email"
-                label="Email Address"
-                name="user_email"
-                autoComplete="email"
-                autoFocus
-                value={user_email}
-                onChange={e => onChange(e)}
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="user_password"
-                label="Password"
-                type="password"
-                id="user_password"
-                autoComplete="current-password"
-                value={user_password}
-                onChange={e => onChange(e)}
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign In
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="/forgotpassword" variant="body2">
-                    Forgot password?
-                  </Link>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="user_email"
+                  label="Email Address"
+                  name="user_email"
+                  autoComplete="email"
+                  autoFocus
+                  value={user_email}
+                  onChange={e => onChange(e)}
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="user_password"
+                  label="Password"
+                  type="password"
+                  id="user_password"
+                  autoComplete="current-password"
+                  value={user_password}
+                  onChange={e => onChange(e)}
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Sign In
+                </Button>
+                <Grid container>
+                  <Grid item xs>
+                    <Link href="/forgotpassword" variant="body2">
+                      Forgot password?
+                    </Link>
+                  </Grid>
+                  <Grid item>
+                    <Link href="/register" variant="body2">
+                      {/* {"Don't have an account? Sign Up"} */}
+                    </Link>
+                  </Grid>
                 </Grid>
-                <Grid item>
-                  <Link href="/register" variant="body2">
-                    {/* {"Don't have an account? Sign Up"} */}
-                  </Link>
-                </Grid>
-              </Grid>
-              <Copyright sx={{ mt: 5 }} />
+                <Copyright sx={{ mt: 5 }} />
+              </Box>
             </Box>
-          </Box>
+          </Grid>
         </Grid>
-      </Grid>
-    </ThemeProvider>
+      </ThemeProvider>
+    </div>
   );
 }
 
